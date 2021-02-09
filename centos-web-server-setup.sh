a17674a4aac213308ee200cb59c51c411257cb6d
bashcarpenter <carpenter@outlook.in>
2021-02-09 13:09:18 +0530

    Create centos-web-server-setup.sh

---
+++ centos-web-server-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
+
+yum install httpd -y;
+
+systemctl enable httpd -y;
+
+wget -O /var/www/html/index.html https://raw.githubusercontent.com/bashcarpenter/chris/main/index.html
+
+systemctl start httpd -y;
+
+
