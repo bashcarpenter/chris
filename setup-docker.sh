33973267e0f4ea929730d89ddece4601f3ec5228
bashcarpenter <carpenter@outlook.in>
2021-07-25 12:55:51 +0530

    Update setup-docker.sh

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,3 +1,4 @@
+# install docker
 apt-get update;
 
 apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y;
@@ -7,3 +8,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update; apt-get install docker-ce docker-ce-cli containerd.io -y;
+
+# create docker swarm
+docker swarm init --advertise-addr 10.124.0.2 --listen-addr 0.0.0.0:2377 --force-new-cluster
+
+# join docker swarm cluster
+docker swarm join --token SWMTKN-1-k378r347yu7fyr3d7dud78dud78 10.124.0.2:2377
+
